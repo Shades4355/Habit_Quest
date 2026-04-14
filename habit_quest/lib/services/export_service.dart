222f20555f867687569697d07731aa57442a3a32
Miguel Echeverry <Miguel_Echeverry@student.uml.edu>
2026-04-14 00:25:17 -0400

    Remove unused share_plus dependency from pubspec.yaml and clean up export_service.dart imports

--- habit_quest/lib/services/export_service.dart
+++ habit_quest/lib/services/export_service.dart
@@ -3,7 +3,6 @@ import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:csv/csv.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:share_plus/share_plus.dart';
 import 'package:file_saver/file_saver.dart';
 import 'package:habit_quest/repositories/habit_repository.dart';
 
