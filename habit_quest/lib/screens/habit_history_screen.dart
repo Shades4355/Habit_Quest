da50122eb6370061629b95d4c0aa4748ad10d5f8
Miguel Echeverry <Miguel_Echeverry@student.uml.edu>
2026-04-27 00:30:51 -0400

    removed unnecessary import

--- habit_quest/lib/screens/habit_history_screen.dart
+++ habit_quest/lib/screens/habit_history_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:habit_quest/interfaces/app_drawer.dart';
-import 'package:habit_quest/interfaces/record_habit_interface_pop_up.dart';
 import 'package:provider/provider.dart';
 import 'package:habit_quest/database/entities/habit_record.dart';
 import 'package:habit_quest/providers/habit_record_provider.dart';
